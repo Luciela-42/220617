23615e340e8dd2f2493c25c7740b06ba3de04eb9
Hvvi_LCLA <inhui99@gmail.com>
2022-06-17 13:05:16 +0900

    220617

--- main.cpp
+++ main.cpp
@@ -11,17 +11,11 @@ int main()
 
 	cout << "입력 된 정수 : " << Num << endl;
 
-	if (Num < 256)
+	for (int i = 7; i >= 0; --i)
 	{
-		for (int i = 7; i >= 0; --i)
-		{
-			int Result = Num >> i & 1;
-			cout << Result;
-		}
+		int Result = Num >> i & 1;
+		cout << Result;
 	}
-	else
-	{
-		cout << "256 미만의 수를 입력 해주세요" << endl;
-	}
+
 	return 0;
 }
